565d8ad52c04395bcfe9effbe6b3a99d1c02127d
zne239 <ari239rc@gmail.com>
2022-06-01 19:25:31 -0300

    variables tipo let

--- script.js
+++ script.js
@@ -5,13 +5,29 @@ console.log(variable_1);
 var variable_2;
 variable_2 = "hello";
 console.log(variable_2);
-console.groupEnd;
+console.groupEnd();
 
 console.groupCollapsed("reasignacion de variables var");
 variable_1 = "HOLA pero en mayuscula";
 console.log(variable_1);
 variable_2 = "HELLO pero en mayuscula";
 console.log(variable_2);
-console.groupEnd;
+console.groupEnd();
+
+console.groupCollapsed("variables let");
+let otraVariable_1 = true;
+console.log(otraVariable_1);
+let otraVariable_2 = true;
+console.log(otraVariable_2);
+console.groupEnd();
+
+console.groupCollapsed("reasignacion de variables let");
+otraVariable_1 = false;
+console.log(otraVariable_1);
+otraVariable_2 = false;
+console.log(otraVariable_2);
+console.groupEnd();
+                                
+
 
                                 
