c2fa163200ffb6642f76d67499fb63475b16ee02
zne239 <ari239rc@gmail.com>
2022-06-01 19:10:55 -0300

    variables var

---
+++ script.js
@@ -0,0 +1,17 @@
+console.groupCollapsed("variables var");
+var variable_1;
+variable_1 = "hola";
+console.log(variable_1);
+var variable_2;
+variable_2 = "hello";
+console.log(variable_2);
+console.groupEnd;
+
+console.groupCollapsed("reasignacion de variables var");
+variable_1 = "HOLA pero en mayuscula";
+console.log(variable_1);
+variable_2 = "HELLO pero en mayuscula";
+console.log(variable_2);
+console.groupEnd;
+
+                                
