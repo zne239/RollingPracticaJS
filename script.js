0febd2cdb57d042d3ee520c83dcce977229ffa2a
zne239 <ari239rc@gmail.com>
2022-06-01 19:59:37 -0300

    objeto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.groupCollapsed("variables var");
+console.groupCollapsed("variables var, ya no se usan");
 var variable_1;
 variable_1 = "hola";
 console.log(variable_1);
@@ -7,31 +7,42 @@ variable_2 = "hello";
 console.log(variable_2);
 console.groupEnd();
 
-console.groupCollapsed("reasignacion de variables var");
-variable_1 = "HOLA pero en mayuscula";
+console.groupCollapsed("reasignacion y redeclaracion var");
+var variable_1 = "HOLA pero en mayuscula";
 console.log(variable_1);
-variable_2 = "HELLO pero en mayuscula";
+var variable_2 = "HELLO pero en mayuscula";
 console.log(variable_2);
 console.groupEnd();
 
-console.groupCollapsed("variables let");
+console.groupCollapsed("variables let, estas chi");
 let otraVariable_1 = true;
 console.log(otraVariable_1);
 let otraVariable_2 = true;
 console.log(otraVariable_2);
 console.groupEnd();
 
-console.groupCollapsed("reasignacion de variables let");
+console.groupCollapsed("reasignacion let");
 otraVariable_1 = false;
 console.log(otraVariable_1);
 otraVariable_2 = false;
 console.log(otraVariable_2);
 console.groupEnd();
 
+console.groupCollapsed("variables const, nada de re");
 const nombre = "Ariel";
 const apellido = "Zane";
-console.log(nombre, apellido)
+console.log(nombre, apellido);
+console.groupEnd();
                                 
+console.groupCollapsed("objeto");
+let unObjeto = {
+    texto: "textito", 
+    numero: 123,
+    booleano: false  
+};
+console.log(unObjeto.texto,unObjeto.numero);
+console.log(unObjeto.booleano)
+console.groupEnd();
 
 
                                 
